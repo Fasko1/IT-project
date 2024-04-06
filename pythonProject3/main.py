9ccbea4bfe57f785e86751826d76ed97a87eac62
huntuzavvvr <kirill05201@mail.ru>
2024-04-07 01:43:19 +0300

    Add print_main_menu function

--- pythonProject3/main.py
+++ pythonProject3/main.py
@@ -56,6 +56,7 @@ def get_order_history(username):
         for order in orders:
             print(f"Order ID: {order[0]}, Pizza: {order[2]}, Cost: {order[3]}, Amount: {order[4]}, Time: {order[5]}")
 
+
 # Function to calculate the cost of the order
 def calculate_cost(pizza_ordered, quantity):
     # Add your own logic here to calculate the cost based on pizza and quantity
@@ -63,57 +64,63 @@ def calculate_cost(pizza_ordered, quantity):
     return cost_per_pizza * quantity
 
 
+def print_main_menu():
+    print("1. Register\n2. Login\nPress 'h' for help")
+
+
 # Main program loop
+def main():
+    print("WELCOME TO FATTY MAN PIZZA ORDERING SYSTEM!")
+    print_main_menu()
 
-print("WELCOME TO FATTY MAN PIZZA ORDERING SYSTEM!")
-print("1. Register")
-print("2. Login")
-print("Press 'h' for help")
+    while True:
 
-while True:
+        choice = input("Enter your choice: ")
 
-    choice = input("Enter your choice: ")
+        if choice == '1':
+            username = input("Enter username: ")
+            password = input("Enter password: ")
+            hashed_password = hashlib.sha256(password.encode()).hexdigest()
+            create_database()  # Create the database if not already exist
+            add_order_to_database(username, hashed_password, "-", 0, datetime.now())
+            print("User registered successfully!")
 
-    if choice == '1':
-        username = input("Enter username: ")
-        password = input("Enter password: ")
-        hashed_password = hashlib.sha256(password.encode()).hexdigest()
-        create_database()  # Create the database if not already exist
-        add_order_to_database(username, hashed_password, "-", 0, datetime.now())
-        print("User registered successfully!")
+        elif choice == '2':
+            username = input("Enter username: ")
+            password = input("Enter password: ")
+            verify_user(username, password)
+            print("(1) Order Pizza")
+            print("(2) Get history")
+            while True:
+                A = input(": ")
+                if A == '2':
+                    get_order_history(username)
+                if A == '1':
+                    basket = ['', 'Pepperoni', 'Margarita','Love']
+                    print("(1) - Pepperoni")
+                    print("(2) - Margarita")
+                    print("(3) - Love")
+                    pizza_ordered = int(input(": "))
+                    pizza_ordered = basket[pizza_ordered]
+                    quantity = 1
+                    time = datetime.now()
 
-    elif choice == '2':
-        username = input("Enter username: ")
-        password = input("Enter password: ")
-        verify_user(username, password)
-        print("(1) Order Pizza")
-        print("(2) Get history")
-        while True:
-            A = input(": ")
-            if A == '2':
-                get_order_history(username)
-            if A == '1':
-                basket = ['', 'Pepperoni', 'Margarita','Love']
-                print("(1) - Pepperoni")
-                print("(2) - Margarita")
-                print("(3) - Love")
-                pizza_ordered = int(input(": "))
-                pizza_ordered = basket[pizza_ordered]
-                quantity = 1
-                time = datetime.now()
+                    hashed_password = hashlib.sha256(password.encode()).hexdigest()
+                    add_order_to_database(username, hashed_password, pizza_ordered, quantity, time)
 
-                hashed_password = hashlib.sha256(password.encode()).hexdigest()
-                add_order_to_database(username, hashed_password, pizza_ordered, quantity, time)
+            pass
 
-        pass
+        elif choice == 'h':
+            print("yes, you need help with your godless pizza eating spree, don't kill yourself you lazy fatty")
+            pass
 
-    elif choice == 'h':
-        print("yes, you need help with your godless pizza eating spree, don't kill yourself you lazy fatty")
-        pass
+        elif choice == '4':
+            print("Thank you for using Pizza Ordering System. Goodbye!")
+            break
 
-    elif choice == '4':
-        print("Thank you for using Pizza Ordering System. Goodbye!")
-        break
+        else:
+            print("Invalid choice. Please try again.")
 
-    else:
-        print("Invalid choice. Please try again.")
+
+if __name__ == '__main__':
+    main()
