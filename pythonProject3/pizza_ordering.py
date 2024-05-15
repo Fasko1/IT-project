4550bac50fdaecb011d953ec6a968da596e7f425
huntuzavvvr <kirill05201@mail.ru>
2024-05-15 16:30:36 +0300

    Add repeat_order and order_id

--- pythonProject3/pizza_ordering.py
+++ pythonProject3/pizza_ordering.py
@@ -1,11 +1,12 @@
 import sqlite3
 from datetime import datetime, timedelta
+from random import randint
 
 
 def create_orders_table():
     conn = sqlite3.connect('pizza_orders.db')
     c = conn.cursor()
-    c.execute('''CREATE TABLE IF NOT EXISTS orders (username text, pizza_ordered text, 
+    c.execute('''CREATE TABLE IF NOT EXISTS orders (id integer unique, username text, pizza_ordered text, 
     quantity integer, time text, delivery_time text, cost real)''')
     conn.commit()
     conn.close()
@@ -77,61 +78,65 @@ def offer(username):
 
 
 def ordering(username):
+
     create_pizzas_table()
     create_orders_table()
     basket = ['', 'Pepperoni', 'Margarita', 'Mushroom pizza']
-
-    offer(username)
-
-    print('(1) - Pepperoni')
-    print('(2) - Margarita')
-    print('(3) - Mushroom pizza')
-    print('To choose the desired ingredients press ENTER')
-    order = list(map(int, input(': ').split()))
-    quantity = 1
-    if order:
-        pizza_ordered = basket[order[0]]
-    else:
-        pizza_ordered = ''
-    if len(order) > 1:
-        quantity = order[1]
-    if pizza_ordered == '':
-        print("Type in your desired ingredients separated by space: ")
-        ingredients = input().split()
-        conn = sqlite3.connect('pizza_orders.db')
-        c = conn.cursor()
-        result = set()
-        for ingredient in ingredients:
-            c.execute('''SELECT title FROM pizzas WHERE ingredients LIKE "%"||?||"%"''', (ingredient,))
-            pizzas = c.fetchall()
-            for i in pizzas:
-                result.add(i[0])
-        if len(result) > 0:
-            print("You would try these pizzas:", ', '.join([i for i in result]))
+    if not repeat_order(username):
+        offer(username)
+        print('(1) - Pepperoni')
+        print('(2) - Margarita')
+        print('(3) - Mushroom pizza')
+        print('To choose the desired ingredients press ENTER')
+        order = list(map(int, input(': ').split()))
+        quantity = 1
+        if order:
+            pizza_ordered = basket[order[0]]
         else:
-            print("Sorry, we have no pizza that satisfies your desires")
-
-    else:
-        if quantity == 0:
-            print("We cannot deliver 0 pizzas")
-        else:
+            pizza_ordered = ''
+        if len(order) > 1:
+            quantity = order[1]
+        if pizza_ordered == '':
+            print("Type in your desired ingredients separated by space: ")
+            ingredients = input().split()
             conn = sqlite3.connect('pizza_orders.db')
             c = conn.cursor()
-            c.execute('''SELECT cost FROM pizzas WHERE title = ?''', (pizza_ordered.lower(),))
-            price = c.fetchone()[0]
-            time = datetime.now().replace(microsecond=0)
-            delivery_time = choose_time(time)
-            add_order_to_database(username, pizza_ordered, quantity, time, delivery_time, price)
-            print("Add ingredients")
+            result = set()
+            for ingredient in ingredients:
+                c.execute('''SELECT title FROM pizzas WHERE ingredients LIKE "%"||?||"%"''', (ingredient,))
+                pizzas = c.fetchall()
+                for i in pizzas:
+                    result.add(i[0])
+            if len(result) > 0:
+                print("You would try these pizzas:", ', '.join([i for i in result]))
+            else:
+                print("Sorry, we have no pizza that satisfies your desires")
+
+        else:
+            if quantity == 0:
+                print("We cannot deliver 0 pizzas")
+            else:
+                conn = sqlite3.connect('pizza_orders.db')
+                c = conn.cursor()
+                c.execute('''SELECT cost FROM pizzas WHERE title = ?''', (pizza_ordered.lower(),))
+                price = c.fetchone()[0]
+                time = datetime.now().replace(microsecond=0)
+                delivery_time = choose_time(time)
+                add_order_to_database(randint(1, 1000000), username, pizza_ordered, quantity, time, delivery_time, price)
+                print("Add ingredients")
 
 
-def add_order_to_database(username, pizza_ordered, quantity, time, delivery_time, price):
+def add_order_to_database(id, username, pizza_ordered, quantity, time, delivery_time, price):
     cost = calculate_cost(quantity, price)
-
     conn = sqlite3.connect('pizza_orders.db')
     c = conn.cursor()
-    c.execute('''INSERT INTO orders VALUES (?, ?, ?, ?, ?, ?)''',
-              (username, pizza_ordered, quantity, str(time), str(delivery_time), cost))
+    while True:
+        try:
+            c.execute('''INSERT INTO orders VALUES (?, ?, ?, ?, ?, ?, ?)''',
+                      (id, username, pizza_ordered, quantity, str(time), str(delivery_time), cost))
+            break
+        except sqlite3.IntegrityError:
+            id = randint(1, 1000000)
     conn.commit()
     conn.close()
     print('Order added to the database successfully!')
@@ -142,3 +147,23 @@ def calculate_cost(quantity, price):
     # Add your own logic here to calculate the cost based on pizza and quantity
     cost = quantity * price
     return cost
+
+
+def repeat_order(username):
+    conn = sqlite3.connect('pizza_orders.db')
+    c = conn.cursor()
+    c.execute('''SELECT * FROM orders WHERE username = ?''', (username,))
+    result = c.fetchall()
+    if result:
+        result = result[-1]
+        pizza_ordered, quantity, price = result[2], result[3], result[6]
+        print(f'Your previous order: {pizza_ordered} in quantity of {quantity}. Cost: {price}. Repeat?')
+        option = input('Yes/No: ').lower()
+        if option == 'yes':
+            time = datetime.now().replace(microsecond=0)
+            delivery_time = choose_time(time)
+            id = randint(1, 1000000)
+            add_order_to_database(id, username, pizza_ordered, quantity, time, delivery_time, price)
+            print("Add ingredients")
+            return True
+    return False
